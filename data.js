d9aab32d21b75927aabf17d7d525d9ea1decd5bd
Riko Primanto Pratama <riko.primanto@cashbac.com>
2023-11-10 18:29:18 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,17 @@
+const nama = "riko primanto";
+let kerjaan = "QA Engineer"
+
+function generateBiodata() {
+    if(kerjaan == "QA Engineer") {
+        // KONDISI PERTAMA
+        console.log('kang ngetes aplikasi');
+    }else {
+        // JIKA KONDISI TIDAK TERPENUHI
+        console.log('riko nganggur');
+    }
+}
+
+console.log(nama);
+console.log(kerjaan);
+
+generateBiodata();
